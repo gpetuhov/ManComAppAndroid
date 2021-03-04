f1c3e1411382e042cd1ffa7729fa612308bf3543
Gennady Petukhov <g3pmail@gmail.com>
2021-03-04 10:57:35 +0300

    Add nav graph

--- app/src/main/java/ru/mancomapp/ui/MainActivity.kt
+++ app/src/main/java/ru/mancomapp/ui/MainActivity.kt
@@ -2,15 +2,15 @@ package ru.mancomapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.mancomapp.BuildConfig
+import androidx.navigation.findNavController
 import ru.mancomapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        app_version.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host).navigateUp()
 }
