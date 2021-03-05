7da2bb76c83672f6eb0a594643e3f047462b39b9
Gennady Petukhov <g3pmail@gmail.com>
2021-03-05 11:30:06 +0300

    Add get credentials info

--- app/src/main/java/ru/mancomapp/ui/login/GetCredentialsFragment.kt
+++ app/src/main/java/ru/mancomapp/ui/login/GetCredentialsFragment.kt
@@ -1,6 +1,8 @@
 package ru.mancomapp.ui.login
 
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,5 +20,17 @@ class GetCredentialsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         welcome_text.text = getString(R.string.welcome_message, getString(R.string.visible_app_name))
+
+        val getCredentialsLink = "<a href=\"${getString(R.string.get_credentials_schedule_url)}\">${getString(R.string.here)}</a>"
+        val html = getString(R.string.get_credentials_info, getCredentialsLink)
+
+        val result = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(html)
+        }
+
+        get_credentials_info.text = result
+        get_credentials_info.movementMethod = LinkMovementMethod.getInstance()
     }
 }
