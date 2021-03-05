7facfc6c60b0580ec22c40642ed944f8ac9cd706
Gennady Petukhov <g3pmail@gmail.com>
2021-03-05 13:35:08 +0300

    Add LoginViewModel

--- app/src/main/java/ru/mancomapp/ui/login/LoginFragment.kt
+++ app/src/main/java/ru/mancomapp/ui/login/LoginFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_login.*
 import ru.mancomapp.R
 import ru.mancomapp.util.extensions.toast
 
 class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
@@ -21,10 +25,20 @@ class LoginFragment : Fragment() {
         welcome_text.text = getString(R.string.welcome_message, getString(R.string.app_name))
 
         login_button.setOnClickListener { onLoginButtonClick() }
+
+        subscribeViewModel()
     }
 
     private fun onLoginButtonClick() {
         // TODO
         toast("Login")
+    }
+
+    private fun subscribeViewModel() {
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        viewModel.isLoginEnabledLiveData.observe(viewLifecycleOwner, Observer { isEnabled ->
+            login_button.isEnabled = isEnabled
+        })
     }
 }
---
+++ app/src/main/java/ru/mancomapp/ui/login/LoginViewModel.kt
@@ -0,0 +1,37 @@
+package ru.mancomapp.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+    var isLoginEnabledLiveData: LiveData<Boolean>
+
+    private val isLoginEnabledLiveDataMutable = MutableLiveData<Boolean>()
+
+    private var login = ""
+    private var password = ""
+    private var isPrivacyPolicyConfirmed = false
+
+    init {
+        isLoginEnabledLiveData = isLoginEnabledLiveDataMutable
+        updateLoginButton()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        // TODO: cancel login request here
+    }
+
+    fun onLoginChanged(login: String) {
+        this.login = login
+        updateLoginButton()
+    }
+
+    private fun updateLoginButton() {
+        val isEnabled = login.isNotEmpty() && password.isNotEmpty() && isPrivacyPolicyConfirmed
+        isLoginEnabledLiveDataMutable.postValue(isEnabled)
+    }
+}
