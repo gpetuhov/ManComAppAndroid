8a6e1962d0a0a739291b834b309bee776f4c9bbf
Gennady Petukhov <g3pmail@gmail.com>
2021-03-05 09:15:23 +0300

    Add MainActivity

--- app/src/main/java/ru/mancomapp/ui/MainActivity.kt
+++ app/src/main/java/ru/mancomapp/ui/MainActivity.kt
@@ -1,16 +1,16 @@
-package ru.mancomapp.ui.login
+package ru.mancomapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import ru.mancomapp.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 
-    override fun onSupportNavigateUp() = findNavController(R.id.nav_host).navigateUp()
+//    override fun onSupportNavigateUp() = findNavController(R.id.nav_host).navigateUp()
 }
