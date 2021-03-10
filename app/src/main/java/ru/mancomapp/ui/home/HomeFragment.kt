4477dae5ee9896b3ab287de20eb56bb0a4b2f393
Gennady Petukhov <g3pmail@gmail.com>
2021-03-10 10:57:35 +0300

    Handle button clicks

--- app/src/main/java/ru/mancomapp/ui/home/HomeFragment.kt
+++ app/src/main/java/ru/mancomapp/ui/home/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
 import ru.mancomapp.R
+import ru.mancomapp.util.extensions.toast
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +18,19 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO
+        request_button.setOnClickListener {
+            // TODO
+            toast(R.string.management_company_requests)
+        }
+
+        security_button.setOnClickListener {
+            // TODO
+            toast(R.string.pass_and_security)
+        }
+
+        payments_button.setOnClickListener {
+            // TODO
+            toast(R.string.payments)
+        }
     }
 }
