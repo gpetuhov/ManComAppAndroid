1009bdc31f8042c9127d389bd1fbdd2dcc9da425
Gennady Petukhov <g3pmail@gmail.com>
2021-03-10 13:01:09 +0300

    Add dummy requests

--- app/src/main/java/ru/mancomapp/ui/requests/RequestsFragment.kt
+++ app/src/main/java/ru/mancomapp/ui/requests/RequestsFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_requests.*
 import ru.mancomapp.R
+import ru.mancomapp.models.Request
 import ru.mancomapp.util.extensions.toast
 
 class RequestsFragment : Fragment() {
+
+    private lateinit var requestsAdapter: RequestsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_requests, container, false)
@@ -29,9 +34,26 @@ class RequestsFragment : Fragment() {
             // TODO
             toast(R.string.order_service)
         }
+
+        requests_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        requestsAdapter = RequestsAdapter()
+        requests_list.adapter = requestsAdapter
+
+        loadDummyRequests()
     }
 
     private fun navigateUp() {
         findNavController().navigateUp()
+    }
+
+    // TODO: remove this
+    private fun loadDummyRequests() {
+        val requests = mutableListOf<Request>()
+
+        (1..100).forEach {
+            requests.add(Request("Заявка $it"))
+        }
+
+        requestsAdapter.setRequestList(requests)
     }
 }
