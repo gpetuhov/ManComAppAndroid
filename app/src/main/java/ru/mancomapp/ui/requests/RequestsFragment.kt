9aa2aef0eaa20884c3b66196b6879bc4c472dfbd
Gennady Petukhov <g3pmail@gmail.com>
2021-03-10 11:47:12 +0300

    Navigate up on back button click

--- app/src/main/java/ru/mancomapp/ui/requests/RequestsFragment.kt
+++ app/src/main/java/ru/mancomapp/ui/requests/RequestsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_requests.*
 import ru.mancomapp.R
 
 class RequestsFragment : Fragment() {
@@ -15,7 +17,10 @@ class RequestsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        back_button.setOnClickListener { navigateUp() }
+    }
 
-        // TODO
+    private fun navigateUp() {
+        findNavController().navigateUp()
     }
 }
