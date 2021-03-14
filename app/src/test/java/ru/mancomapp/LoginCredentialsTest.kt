5a9290a8112efffcee5ee17ece4c38d3f1101cbb
Gennady Petukhov <g3pmail@gmail.com>
2021-03-14 15:05:01 +0300

    Add LoginCredentials test

---
+++ app/src/test/java/ru/mancomapp/LoginCredentialsTest.kt
@@ -0,0 +1,46 @@
+package ru.mancomapp
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import ru.mancomapp.ui.login.LoginViewModel
+
+class LoginCredentialsTest {
+
+    companion object {
+        private const val LOGIN = "login"
+        private const val PASSWORD = "password"
+    }
+
+    private lateinit var loginCredentials: LoginViewModel.LoginCredentials
+
+    @Before
+    fun initLoginCredentials() {
+        loginCredentials = LoginViewModel.LoginCredentials()
+    }
+
+    @Test
+    fun isEmpty_emptyLoginEmptyPassword_true() {
+        assertTrue(loginCredentials.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_emptyLogin_true() {
+        loginCredentials.password = PASSWORD
+        assertTrue(loginCredentials.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_emptyPassword_true() {
+        loginCredentials.login = LOGIN
+        assertTrue(loginCredentials.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_notEmptyLoginNotEmptyPassword_false() {
+        loginCredentials.login = LOGIN
+        loginCredentials.password = PASSWORD
+        assertFalse(loginCredentials.isEmpty())
+    }
+}
