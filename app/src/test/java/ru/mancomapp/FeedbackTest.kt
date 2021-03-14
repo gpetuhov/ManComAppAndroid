dc7d71e065910f4be103158545ae44af7a4354b0
Gennady Petukhov <g3pmail@gmail.com>
2021-03-14 15:44:21 +0300

    Add FeedbackTest

---
+++ app/src/test/java/ru/mancomapp/FeedbackTest.kt
@@ -0,0 +1,46 @@
+package ru.mancomapp
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import ru.mancomapp.ui.feedback.FeedbackViewModel
+
+class FeedbackTest {
+
+    companion object {
+        private const val TITLE = "Title"
+        private const val CONTENT = "Content"
+    }
+
+    private lateinit var feedback: FeedbackViewModel.Feedback
+
+    @Before
+    fun initFeedback() {
+        feedback = FeedbackViewModel.Feedback()
+    }
+
+    @Test
+    fun isEmpty_emptyTitleEmptyContent_true() {
+        assertTrue(feedback.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_emptyTitle_true() {
+        feedback.content = CONTENT
+        assertTrue(feedback.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_EmptyContent_true() {
+        feedback.title = TITLE
+        assertTrue(feedback.isEmpty())
+    }
+
+    @Test
+    fun isEmpty_notEmptyTitleNotEmptyContent_false() {
+        feedback.title = TITLE
+        feedback.content = CONTENT
+        assertFalse(feedback.isEmpty())
+    }
+}
