5c8f1c401569a5559b6be5cb2a5e48088696bb02
Gennady Petukhov <g3pmail@gmail.com>
2021-03-16 13:10:58 +0300

    Add RequestsViewModel tests

--- app/src/test/java/ru/mancomapp/di/components/TestAppComponent.kt
+++ app/src/test/java/ru/mancomapp/di/components/TestAppComponent.kt
@@ -4,11 +4,13 @@ import dagger.Component
 import ru.mancomapp.di.modules.TestAppModule
 import ru.mancomapp.presentation.feedback.FeedbackViewModel
 import ru.mancomapp.presentation.login.LoginViewModel
+import ru.mancomapp.presentation.requests.RequestsViewModel
 import javax.inject.Singleton
 
 @Component(modules = [TestAppModule::class])
 @Singleton
 interface TestAppComponent : AppComponent {
     override fun inject(loginViewModel: LoginViewModel)
+    override fun inject(requestsViewModel: RequestsViewModel)
     override fun inject(feedbackViewModel: FeedbackViewModel)
 }
