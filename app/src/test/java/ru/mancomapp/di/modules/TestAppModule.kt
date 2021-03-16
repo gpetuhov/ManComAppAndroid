75c9b2a0f33ddebfce32a7f253890b16746114d1
Gennady Petukhov <g3pmail@gmail.com>
2021-03-16 12:03:41 +0300

    Add FeedbackUseCase tests

--- app/src/test/java/ru/mancomapp/di/modules/TestAppModule.kt
+++ app/src/test/java/ru/mancomapp/di/modules/TestAppModule.kt
@@ -3,10 +3,12 @@ package ru.mancomapp.di.modules
 import dagger.Module
 import dagger.Provides
 import org.mockito.Mockito
+import ru.mancomapp.data.repository.FeedbackRepository
 import ru.mancomapp.data.repository.LoginRepository
 import ru.mancomapp.data.repository.RequestRepository
 import ru.mancomapp.domain.usecase.login.LoginUseCase
 import ru.mancomapp.domain.usecase.RequestUseCase
+import ru.mancomapp.domain.usecase.feedback.FeedbackUseCase
 import javax.inject.Singleton
 
 @Module
@@ -14,18 +16,31 @@ class TestAppModule {
 
     @Provides
     @Singleton
-    fun providesLoginRepository(): LoginRepository = Mockito.mock(LoginRepository::class.java)
+    fun providesLoginRepository(): LoginRepository =
+        Mockito.mock(LoginRepository::class.java)
 
     @Provides
     @Singleton
-    fun providesLoginUseCase(loginRepository: LoginRepository) = LoginUseCase(loginRepository)
+    fun providesLoginUseCase(loginRepository: LoginRepository) =
+        LoginUseCase(loginRepository)
 
     @Provides
     @Singleton
-    fun providesRequestRepository() = RequestRepository()
+    fun providesRequestRepository(): RequestRepository =
+        Mockito.mock(RequestRepository::class.java)
 
     @Provides
     @Singleton
     fun providesRequestUseCase(requestRepository: RequestRepository) =
         RequestUseCase(requestRepository)
+
+    @Provides
+    @Singleton
+    fun providesFeedbackRepository(): FeedbackRepository =
+        Mockito.mock(FeedbackRepository::class.java)
+
+    @Provides
+    @Singleton
+    fun providesFeedbackUseCase(feedbackRepository: FeedbackRepository) =
+        FeedbackUseCase(feedbackRepository)
 }
