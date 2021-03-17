a5bbb2e624d6315f5a55ad8d275a58bb720b0857
Gennady Petukhov <g3pmail@gmail.com>
2021-03-17 13:18:45 +0300

    Show security requests list

--- app/src/main/java/ru/mancomapp/presentation/security/SecurityFragment.kt
+++ app/src/main/java/ru/mancomapp/presentation/security/SecurityFragment.kt
@@ -5,17 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_security.*
-import kotlinx.android.synthetic.main.fragment_security.back_button
 import ru.mancomapp.R
+import ru.mancomapp.domain.models.request.Request
 import ru.mancomapp.presentation.requests.RequestsAdapter
+import ru.mancomapp.utils.extensions.setVisible
+import ru.mancomapp.utils.extensions.toast
 
 class SecurityFragment : Fragment() {
 
+    private lateinit var viewModel: SecurityViewModel
     private lateinit var requestsAdapter: RequestsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -25,6 +29,8 @@ class SecurityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        subscribeViewModel()
+
         back_button.setOnClickListener { navigateUp() }
         person_pass_button.setOnClickListener { navigateToPersonPassFragment() }
         car_pass_button.setOnClickListener { navigateToCarPassFragment() }
@@ -32,10 +38,37 @@ class SecurityFragment : Fragment() {
         security_requests_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         requestsAdapter = RequestsAdapter()
         security_requests_list.adapter = requestsAdapter
+
+        viewModel.loadRequestHistory()
+    }
+
+    private fun subscribeViewModel() {
+        viewModel = ViewModelProvider(this).get(SecurityViewModel::class.java)
+        viewModel.isRequestHistoryLoading.observe(viewLifecycleOwner, { isLoading -> showProgress(isLoading) })
+        viewModel.requestHistoryError.observe(viewLifecycleOwner, { errorMessageId -> toast(errorMessageId) })
+        viewModel.requestHistory.observe(viewLifecycleOwner, { requests -> updateRequestsUI(requests) })
     }
 
     private fun navigateUp() {
         findNavController().navigateUp()
+    }
+
+    private fun updateRequestsUI(requests: List<Request>) {
+        requestsAdapter.submitList(requests)
+        showRequests(requests.isNotEmpty())
+        showRequestsEmpty(requests.isEmpty())
+    }
+
+    private fun showProgress(isVisible: Boolean) {
+        load_security_requests_progress.setVisible(isVisible)
+    }
+
+    private fun showRequests(isVisible: Boolean) {
+        security_requests_list.setVisible(isVisible)
+    }
+
+    private fun showRequestsEmpty(isVisible: Boolean) {
+        security_requests_empty.setVisible(isVisible)
     }
 
     private fun navigateToPersonPassFragment() {
