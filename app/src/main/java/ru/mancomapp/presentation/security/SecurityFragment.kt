ae28e12a01acad6bec9afefc8009226d2db3811e
Gennady Petukhov <g3pmail@gmail.com>
2021-03-17 12:57:55 +0300

    Add dummy security requests

--- app/src/main/java/ru/mancomapp/presentation/security/SecurityFragment.kt
+++ app/src/main/java/ru/mancomapp/presentation/security/SecurityFragment.kt
@@ -7,10 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_security.*
+import kotlinx.android.synthetic.main.fragment_security.back_button
 import ru.mancomapp.R
+import ru.mancomapp.domain.models.request.CarPassAccessType
+import ru.mancomapp.domain.models.request.PersonPassAccessType
+import ru.mancomapp.domain.models.request.Request
+import ru.mancomapp.domain.models.request.RequestStatus
+import ru.mancomapp.presentation.requests.RequestsAdapter
 
 class SecurityFragment : Fragment() {
+
+    private lateinit var requestsAdapter: RequestsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_security, container, false)
@@ -22,6 +32,13 @@ class SecurityFragment : Fragment() {
         back_button.setOnClickListener { navigateUp() }
         person_pass_button.setOnClickListener { navigateToPersonPassFragment() }
         car_pass_button.setOnClickListener { navigateToCarPassFragment() }
+
+        security_requests_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        requestsAdapter = RequestsAdapter()
+        security_requests_list.adapter = requestsAdapter
+
+        // TODO: remove this
+        requestsAdapter.submitList(getDummyRequests())
     }
 
     private fun navigateUp() {
@@ -40,5 +57,35 @@ class SecurityFragment : Fragment() {
 
     private fun navigate(action: NavDirections) {
         findNavController().navigate(action)
+    }
+
+    // TODO: remove this
+    private fun getDummyRequests(): List<Request> {
+        val requests = mutableListOf<Request>()
+
+        (1..100).forEach { index ->
+            val request: Request
+
+            if (index % 2 == 0) {
+                request = Request.PersonPass().apply {
+                    id = index
+                    personName = "Иванов Иван Иванович"
+                    accessType = PersonPassAccessType.APARTMENT
+                    status = RequestStatus.NEW
+                }
+            } else {
+                request = Request.CarPass().apply {
+                    id = index
+                    carModel = "Мерседес"
+                    carNumber = "X000XX000"
+                    accessType = CarPassAccessType.GUEST_PARKING
+                    status = RequestStatus.ON_REVIEW
+                }
+            }
+
+            requests.add(request)
+        }
+
+        return requests
     }
 }
