5340e307a6ea84ee42a357bf9c46b8ef0338f360
Gennady Petukhov <g3pmail@gmail.com>
2021-03-18 15:04:19 +0300

    Refactor PersonPassTest

--- app/src/test/java/ru/mancomapp/pass/PersonPassTest.kt
+++ app/src/test/java/ru/mancomapp/pass/PersonPassTest.kt
@@ -4,7 +4,6 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import ru.mancomapp.domain.models.pass.PassDate
 import ru.mancomapp.domain.models.pass.PersonPass
 import ru.mancomapp.domain.models.pass.PersonPassAccessType
 
@@ -12,19 +11,14 @@ class PersonPassTest {
 
     companion object {
         private const val NAME = "Name"
-        private const val YEAR = 2021
-        private const val MONTH = 3
-        private const val DAY = 18
-        private const val TIME_IN_MILLIS = 1616056739495
+        private val PASS_DATE = PersonPassTestData.getPassDate()
     }
 
     private lateinit var personPass: PersonPass
-    private lateinit var passDate: PassDate
 
     @Before
-    fun initFeedback() {
+    fun init() {
         personPass = PersonPass()
-        initPassDate()
     }
 
     @Test
@@ -34,7 +28,7 @@ class PersonPassTest {
 
     @Test
     fun isEmpty_emptyName_true() {
-        personPass.passDate = passDate
+        personPass.passDate = PASS_DATE
         personPass.accessType = PersonPassAccessType.OTHER
         assertTrue(personPass.isEmpty())
     }
@@ -49,23 +43,15 @@ class PersonPassTest {
     @Test
     fun isEmpty_accessTypeNotSelected_true() {
         personPass.personName = NAME
-        personPass.passDate = passDate
+        personPass.passDate = PASS_DATE
         assertTrue(personPass.isEmpty())
     }
 
     @Test
     fun isEmpty_notEmpty_false() {
         personPass.personName = NAME
-        personPass.passDate = passDate
+        personPass.passDate = PASS_DATE
         personPass.accessType = PersonPassAccessType.OTHER
         assertFalse(personPass.isEmpty())
-    }
-
-    private fun initPassDate() {
-        passDate = PassDate()
-        passDate.year = YEAR
-        passDate.month = MONTH
-        passDate.day = DAY
-        passDate.timeInMillis = TIME_IN_MILLIS
     }
 }
