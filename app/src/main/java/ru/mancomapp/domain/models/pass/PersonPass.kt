8d95259175e23979346af15f5451380462ec28e7
Gennady Petukhov <g3pmail@gmail.com>
2021-03-18 12:31:45 +0300

    Add PersonPass tests

--- app/src/main/java/ru/mancomapp/domain/models/pass/PersonPass.kt
+++ app/src/main/java/ru/mancomapp/domain/models/pass/PersonPass.kt
@@ -2,6 +2,12 @@ package ru.mancomapp.domain.models.pass
 
 class PersonPass {
     var personName: String = ""
-    val passDate: PassDate = PassDate()
+    var passDate: PassDate = PassDate()
     var accessType: PersonPassAccessType = PersonPassAccessType.NOT_SELECTED
+
+    fun isEmpty(): Boolean {
+        return personName.isEmpty()
+                || passDate.isEmpty()
+                || accessType == PersonPassAccessType.NOT_SELECTED
+    }
 }
