895d6b2ceb2cd2040da014c0981fc3eb1a304443
Gennady Petukhov <g3pmail@gmail.com>
2021-03-19 12:43:34 +0300

    Change request date empty month value

--- app/src/main/java/ru/mancomapp/domain/models/request/RequestDate.kt
+++ app/src/main/java/ru/mancomapp/domain/models/request/RequestDate.kt
@@ -2,9 +2,9 @@ package ru.mancomapp.domain.models.request
 
 class RequestDate {
     var year: Int = 0
-    var month: Int = 0
+    var month: Int = -1
     var day: Int = 0
     var timeInMillis: Long = 0
 
-    fun isEmpty() = year == 0 || month == 0 || day == 0 || timeInMillis == 0L
+    fun isEmpty() = year == 0 || month == -1 || day == 0 || timeInMillis == 0L
 }
