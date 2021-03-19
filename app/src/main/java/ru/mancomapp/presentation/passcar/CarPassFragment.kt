45d0af483393a58f9fa85bbf454c5a7050cd12d7
Gennady Petukhov <g3pmail@gmail.com>
2021-03-19 10:25:02 +0300

    Handle CarPassFragment button clicks

--- app/src/main/java/ru/mancomapp/presentation/passcar/CarPassFragment.kt
+++ app/src/main/java/ru/mancomapp/presentation/passcar/CarPassFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_car_pass.*
 import ru.mancomapp.R
+import ru.mancomapp.utils.extensions.toast
 
 class CarPassFragment : Fragment() {
 
@@ -16,6 +19,28 @@ class CarPassFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        back_button.setOnClickListener { navigateUp() }
+        car_pass_date_input.setOnClickListener { onSelectDateClick() }
+        car_pass_access_type.setOnClickListener { onSelectAccessTypeClick() }
+        car_pass_send_button.setOnClickListener { onSendButtonClick() }
+    }
+
+    private fun navigateUp() {
+        findNavController().navigateUp()
+    }
+
+    private fun onSelectDateClick() {
         // TODO
+        toast("Select date")
+    }
+
+    private fun onSelectAccessTypeClick() {
+        // TODO: implement
+        toast("Select access type")
+    }
+
+    private fun onSendButtonClick() {
+        // TODO
+        toast("Send")
     }
 }
