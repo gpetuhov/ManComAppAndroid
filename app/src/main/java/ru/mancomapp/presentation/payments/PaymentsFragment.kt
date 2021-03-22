8d3dd0cb08dc0781f58316891781c56764532f9a
Gennady Petukhov <g3pmail@gmail.com>
2021-03-22 11:21:56 +0300

    Add payments back button

--- app/src/main/java/ru/mancomapp/presentation/payments/PaymentsFragment.kt
+++ app/src/main/java/ru/mancomapp/presentation/payments/PaymentsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_payments.*
 import ru.mancomapp.R
 
 class PaymentsFragment : Fragment() {
@@ -16,6 +18,10 @@ class PaymentsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO
+        back_button.setOnClickListener { navigateUp() }
+    }
+
+    private fun navigateUp() {
+        findNavController().navigateUp()
     }
 }
